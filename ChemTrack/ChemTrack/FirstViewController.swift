9f6aed8e0bbf917ed89818d12a4b08c77ce7d097
ChristianCornelis <ccorneli@mail.uoguelph.ca>
2018-03-26 16:15:17 -0400

    added hello world

---
+++ ChemTrack/ChemTrack/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  ChemTrack
+//
+//  Created by Christian Cornelis on 2018-03-26.
+//  Copyright © 2018 Christian Cornelis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print ("hello world")
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
